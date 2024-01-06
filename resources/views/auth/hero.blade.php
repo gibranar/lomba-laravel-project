c60d874bf725dfa513747f6cfd6c4c92772c8cac
wahyuartha07 <129440110+wahyuartha07@users.noreply.github.com>
2024-01-06 22:27:34 +0700

    update landing

---
+++ resources/views/auth/hero.blade.php
@@ -0,0 +1,15 @@
+<section id="hero" class="d-flex align-items-center">
+    <div class="container" data-aos="zoom-out" data-aos-delay="100">
+        <h1>Lomba Inovasi Program Pemberdayaan<span>dan Pembangunan Desa</span></h1>
+        <h2>LIP3D merupakan wadah dan ajang lomba yang diharapkan mampu mendorong pemikiran/gagasan
+            mahasiswa di Indonesia yang unggul, kreatif, inovatif dan berkelanjutan dalam mengembang kemiskinan
+            di wilayahnya masing-masing
+        </h2>
+        <div class="d-flex">
+            <a href="#about" class="btn-get-started scrollto">Daftar Lomba</a>
+            <a href="#about" class="btn-get-started scrollto ms-4">Masuk Sistem</a>
+            <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video ms-4 mt-2"><i
+                    class="bi bi-play-circle"></i><span>Watch Video Registration</span></a>
+        </div>
+    </div>
+</section>
